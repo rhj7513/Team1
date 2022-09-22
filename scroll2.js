e87b6be58c948f2bff57a2920b1a503b08d0b292
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2022-09-22 21:16:12 +0900

    Delete scroll2.js

--- scroll2.js
+++
@@ -1,8 +0,0 @@
-// $(".m").click(function(event){
-//     event.preventDefault();
-//     x = $(this).attr("href");
-//         $("html,body").stop().animate({scrollTop : $(x).offset().top - 130}, 1000, "easeInOutExpo");
-// });
-
-
-
